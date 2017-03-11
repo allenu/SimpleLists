610ffbcb9835b0f940425b0f59c4cf603d30f202
Allen Ussher <allenu@gmail.com>
2017-03-11 14:26:42 -0800

    Initial Commit

---
+++ SimpleLists/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleLists
+//
+//  Created by Allen Ussher on 3/11/17.
+//  Copyright © 2017 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimpleLists/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleLists
+//
+//  Created by Allen Ussher on 3/11/17.
+//  Copyright © 2017 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
